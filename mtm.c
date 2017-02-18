9ff4500b8e54d3b232864724e7cd5c6d5fda1986
Rob King <jking@deadpixi.com>
2017-02-18 14:32:55 -0600

    Style fix.

--- mtm.c
+++ mtm.c
@@ -393,8 +393,7 @@ getinput(NODE *n, void *p)
     if (n->pt >= 0 && FD_ISSET(n->pt, (fd_set *)p)){
         char buf[BUFMAX + 1] = {0};
         ssize_t r = read(n->pt, buf, BUFMAX);
-        if (r < 0)
-            return errno == EINTR? true : (deletenode(n), false);
+        if (r < 0) return errno == EINTR? true : (deletenode(n), false);
         tmt_writemb(n->vt, buf, r);
     }
 
