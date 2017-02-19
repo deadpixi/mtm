7232ec3a3b65cadf0e40bfd8c71193097585efe9
Rob King <jking@deadpixi.com>
2017-02-18 21:47:56 -0600

    Style fixes.

--- config.h
+++ config.h
@@ -1,18 +1,25 @@
-// Command key
+/* The following definitions change the keys used to send commands
+ * to mtm. In all cases, the value must be something that ncurses's
+ * getch(3) function could return.
+ */
+
+/* The default command prefix key, when modified by cntrl.
+ * This can be changed at runtime using the '-c' flag.
+ */
 #define COMMAND_KEY 'g'
 
-// Basic motions
-#define MOVE_UP KEY_UP
-#define MOVE_DOWN KEY_DOWN
+/* The change focus keys. */
+#define MOVE_UP    KEY_UP
+#define MOVE_DOWN  KEY_DOWN
 #define MOVE_RIGHT KEY_RIGHT
-#define MOVE_LEFT KEY_LEFT
+#define MOVE_LEFT  KEY_LEFT
 
-// Split commands
+/* The split terminal keys. */
 #define SPLIT_HORIZONTAL 'h'
 #define SPLIT_VERTICAL 'v'
 
-// Delete a node
+/* The delete terminal key. */
 #define DELETE_NODE 'w'
 
-// Redraw
+/* The force redraw key. */
 #define REDRAW 'l'
