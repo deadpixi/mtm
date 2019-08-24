c1a10edc5dd750234ebe3252c848849401f4b59d
Rob King <jking@deadpixi.com>
2019-08-23 21:59:33 -0500

    Revert "Switch to a table-driven state machine."
    This reverts commit cb6925e9947a2874c9e479c3ce8d105dbdc1b0b5.

--- vtparser.c
+++ vtparser.c
@@ -33,6 +33,7 @@
 
 typedef struct ACTION ACTION;
 struct ACTION{
+    wchar_t lo, hi;
     void (*cb)(VTPARSER *p, wchar_t w);
     STATE *next;
 };
@@ -119,18 +120,14 @@ static void
 handlechar(VTPARSER *vp, wchar_t w)
 {
     vp->s = vp->s? vp->s : &ground;
-    if (w >= MAXACTIONS){
-        doprint(vp, w);
-        return;
-    }
-
-    const ACTION *a = &vp->s->actions[w];
-    if (a->cb)
+    for (ACTION *a = vp->s->actions; a->cb; a++) if (w >= a->lo && w <= a->hi){
         a->cb(vp, w);
-    if (a->next){
-        vp->s = a->next;
-        if (a->next->entry)
-            a->next->entry(vp);
+        if (a->next){
+            vp->s = a->next;
+            if (a->next->entry)
+                a->next->entry(vp);
+        }
+        return;
     }
 }
 
@@ -160,5 +157,86 @@ vtwrite(VTPARSER *vp, const char *s, size_t n)
     }
 }
 
-/* Include the generated state tables. */
-#include "vttable.c"
+/**** STATE DEFINITIONS
+ * This was built by consulting the excellent state chart created by
+ * Paul Flo Williams: http://vt100.net/emu/dec_ansi_parser
+ * Please note that Williams does not (AFAIK) endorse this work.
+ */
+#define MAKESTATE(name, onentry, ...)         \
+    static STATE name ={                      \
+        onentry ,                             \
+        {                                     \
+            {0x00, 0x00, ignore,    NULL},    \
+            {0x7f, 0x7f, ignore,    NULL},    \
+            {0x18, 0x18, docontrol, &ground}, \
+            {0x1a, 0x1a, docontrol, &ground}, \
+            {0x1b, 0x1b, ignore,    &escape}, \
+            {0x01, 0x06, docontrol, NULL},    \
+            {0x08, 0x17, docontrol, NULL},    \
+            {0x19, 0x19, docontrol, NULL},    \
+            {0x1c, 0x1f, docontrol, NULL},    \
+            __VA_ARGS__ ,                     \
+            {0x07, 0x07, docontrol, NULL},    \
+            {0x00, 0x00, NULL,      NULL}     \
+        }                                     \
+    }
+
+MAKESTATE(ground, NULL,
+    {0x20, WCHAR_MAX, doprint, NULL}
+);
+
+MAKESTATE(escape, reset,
+    {0x21, 0x21, ignore,   &osc_string},
+    {0x20, 0x2f, collect,  &escape_intermediate},
+    {0x30, 0x4f, doescape, &ground},
+    {0x51, 0x57, doescape, &ground},
+    {0x59, 0x59, doescape, &ground},
+    {0x5a, 0x5a, doescape, &ground},
+    {0x5c, 0x5c, doescape, &ground},
+    {0x6b, 0x6b, ignore,   &osc_string},
+    {0x60, 0x7e, doescape, &ground},
+    {0x5b, 0x5b, ignore,   &csi_entry},
+    {0x5d, 0x5d, ignore,   &osc_string},
+    {0x5e, 0x5e, ignore,   &osc_string},
+    {0x50, 0x50, ignore,   &osc_string},
+    {0x5f, 0x5f, ignore,   &osc_string}
+);
+
+MAKESTATE(escape_intermediate, NULL,
+    {0x20, 0x2f, collect,  NULL},
+    {0x30, 0x7e, doescape, &ground}
+);
+
+MAKESTATE(csi_entry, reset,
+    {0x20, 0x2f, collect, &csi_intermediate},
+    {0x3a, 0x3a, ignore,  &csi_ignore},
+    {0x30, 0x39, param,   &csi_param},
+    {0x3b, 0x3b, param,   &csi_param},
+    {0x3c, 0x3f, collect, &csi_param},
+    {0x40, 0x7e, docsi,   &ground}
+);
+
+MAKESTATE(csi_ignore, NULL,
+    {0x20, 0x3f, ignore, NULL},
+    {0x40, 0x7e, ignore, &ground}
+);
+
+MAKESTATE(csi_param, NULL,
+    {0x30, 0x39, param,   NULL},
+    {0x3b, 0x3b, param,   NULL},
+    {0x3a, 0x3a, ignore,  &csi_ignore},
+    {0x3c, 0x3f, ignore,  &csi_ignore},
+    {0x20, 0x2f, collect, &csi_intermediate},
+    {0x40, 0x7e, docsi,   &ground}
+);
+
+MAKESTATE(csi_intermediate, NULL,
+    {0x20, 0x2f, collect, NULL},
+    {0x30, 0x3f, ignore,  &csi_ignore},
+    {0x40, 0x7e, docsi,   &ground}
+);
+
+MAKESTATE(osc_string, reset,
+    {0x07, 0x07, doosc, &ground},
+    {0x20, 0x7f, collectosc, NULL}
+);
